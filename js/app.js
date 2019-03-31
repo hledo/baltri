6ec73adaf13ad750cbc1c9510419b4d18daff4f5
Hector <hledo@users.noreply.github.com>
2019-03-30 20:24:29 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,6 +3,44 @@ angular.module("mainApp", [])
     $scope.firstName = "John";
     $scope.lastName = "Doe";
     $scope.section = "nosotros";
+
+
+    $scope.articles = [
+    	{
+    		name: "Llavero Baltri",
+    		picture: "img/baltri_llavero.png",
+    		description: ""
+    	},
+    	{
+    		name: "Tank top La Fuerza",
+    		picture: "img/lafuerza_tank.png",
+    		description: ""
+    	},
+    	{
+    		name: "Pin Crika",
+    		picture: "img/crika_pin.png",
+    		description: ""
+    	},
+    	{
+    		name: "Tshirt Te Dije 100 Mil Dolare",
+    		picture: "img/tedije100mil_tshirt.png",
+    		description: ""
+    	},
+    	{
+    		name: "Pack de Stickers",
+    		picture: "img/stickerpack.png",
+    		description: ""
+    	},
+    	{
+    		name: "Carteras Varias",
+    		picture: "img/carteras.png",
+    		description: ""
+    	}
+
+    ];
+
+
+
     $scope.sectionChange = function (e) {
     	$scope.section = e.currentTarget.id;
     };
